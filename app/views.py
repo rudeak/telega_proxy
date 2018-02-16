d4f49bd6a3c016503ad410712ea7b22422d88fce
rudeak <rudeak@gmail.com>
2018-02-16 16:34:48 +0200

    дадана логін форма - не працює дадні шаблони

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'nickname': 'Miguel' } # выдуманный пользователь
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
+
+app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form)
