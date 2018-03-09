fa1617c5a9940ff2a3ccc9eb6ff43da9525b2648
rudeak <rudeak@gmail.com>
2018-03-09 21:38:25 +0200

    ['chat_name']

--- app/game_managment.py
+++ app/game_managment.py
@@ -22,7 +22,7 @@ def active_games_list (owner = 0):
         games = Game.query.filter_by(owner = owner).all()
     game_table = []
     for game in games:
-        t = {'game_domain':game.game_domain, 'game_id':game.game_id,'game_name':game.game_name,'gamer':game.gamer,'login':return_gamer_name(game.gamer),'chat':game.chat,'chat_name':return_chat_name (game.chat).chat_name, 'tg_id':return_chat_name (game.chat).chat_tg_id}
+        t = {'game_domain':game.game_domain, 'game_id':game.game_id,'game_name':game.game_name,'gamer':game.gamer,'login':return_gamer_name(game.gamer),'chat':game.chat,'chat_name':return_chat_name (game.chat)['chat_name'], 'tg_id':return_chat_name (game.chat)['chat_tg_id']}
         game_table.append (t)
     return game_table
 
