06a0ccbe0ab3513bd44f5fcb51c099fb44709e96
rudeak <rudeak@gmail.com>
2018-03-10 01:06:56 +0200

    dgfsdfg

--- app/proxy/proxy.py
+++ app/proxy/proxy.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
-from flask_login import current_user
+from flask_login import current_user, login_required
 import app
 
 
@@ -8,6 +8,7 @@ proxy = Blueprint('proxy', 'proxy', template_folder='templates')
 
 
 @proxy.route('/')
+@login_required
 def show():
-    return app.current_user.login
+    return 'hello'
     
