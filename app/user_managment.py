31c065fff3ca2d5e59a29d2d2ca7b5351a2fc20c
rudeak <rudeak@gmail.com>
2018-03-10 01:26:46 +0200

    role edit

--- app/user_managment.py
+++ app/user_managment.py
@@ -35,7 +35,7 @@ def users_list():
 def edit_role (id, role):
     user = User.query.filter_by (id=id).first()
     user.role = role
-    if id == 0:
+    if id == 1:
        user.role = 2 
     try:
         db.session.commit()
