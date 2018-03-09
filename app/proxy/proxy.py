798bdaf81732d3a780049106c0160fb703aca929
rudeak <rudeak@gmail.com>
2018-03-10 00:43:14 +0200

    current user import to proxy

--- app/proxy/proxy.py
+++ app/proxy/proxy.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
-from app import db
+from flask_login import current_user
+
 
 proxy = Blueprint('proxy', 'proxy', template_folder='templates')
 
 
 @proxy.route('/')
 def show(page):
-    return 'hello world'
+    return current_user.id
