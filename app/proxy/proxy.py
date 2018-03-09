8702f0c45ed719df9b72338f2184dbcd51c097de
rudeak <rudeak@gmail.com>
2018-03-10 01:15:07 +0200

    render template

--- app/proxy/proxy.py
+++ app/proxy/proxy.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, abort, g
+from flask import Blueprint, render_template, flash, redirect, session, url_for, request, g
 from jinja2 import TemplateNotFound
-from flask_login import current_user, login_required
+from flask_login import login_user, logout_user, current_user, login_required
 import app
 
 
@@ -10,5 +10,5 @@ proxy = Blueprint('proxy', 'proxy', template_folder='templates')
 @proxy.route('/')
 @login_required
 def show():
-    return g.user.login
+    return render_template ('index.html', user = current_user)
     
