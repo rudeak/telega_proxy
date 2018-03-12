85e8849543b2f3298ef981b9eae103dc1a9410e6
rudeak <rudeak@gmail.com>
2018-03-12 17:18:24 +0200

    imports

--- app/game_managment.py
+++ app/game_managment.py
@@ -96,5 +96,15 @@ def proxy_db(id):
         db.session.rollback()
         return 'помилка запису параметрів проксі в базу данних'
 
-
+def edit_game_name (id, name):
+    game = Game.query.filter_by (game_id = id).first()
+    game.game_name = name
+    print ('new game name ='+ name)
+    try:
+        print (game.game_name)
+        db.session.commit()
+        return 1
+    except:
+        db.session.rollback()
+        return 'помилка редагування назви гри'
 
