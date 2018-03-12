ec8750c516b89e059f23983ac69ee8110ccd3f99
rudeak <rudeak@gmail.com>
2018-03-12 21:11:02 +0200

    imports moved

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app.proxy.proxy import proxy
-from app.bot.bot import bot
-from app.api.api import api
 import pymysql
 
 pymysql.install_as_MySQLdb()
@@ -11,9 +8,6 @@ pymysql.install_as_MySQLdb()
 app = Flask(__name__)
 app.config.from_object('config')
 app.config['TESTING'] = False
-app.register_blueprint(proxy , url_prefix='/proxy')
-app.register_blueprint(bot , url_prefix='/bot')
-app.register_blueprint(api , url_prefix='/api')
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
@@ -22,5 +16,11 @@ lm.login_view = 'login'
 from app import views
 from app import forms
 from app import models
+from app.proxy.proxy import proxy
+from app.bot.bot import bot
+from app.api.api import api
 from app.proxy.parser import get_game_info
+app.register_blueprint(proxy , url_prefix='/proxy')
+app.register_blueprint(bot , url_prefix='/bot')
+app.register_blueprint(api , url_prefix='/api')
 #from app.proxy.game_controller import edit_game_name
