1a4acf4f3d811a58eb94c952b90a6ef747a0e6c8
rudeak <rudeak@gmail.com>
2018-03-12 17:04:26 +0200

    fsdf

--- app/game_managment.py
+++ app/game_managment.py
@@ -99,7 +99,9 @@ def proxy_db(id):
 def edit_game_name (id, name):
     game = Game.query.filter_by (game_id = id).first()
     game.game_name = name
+    print ('new game name ='+ name)
     try:
+        print (game.game_name)
         db.session.commit()
         return 1
     except:
