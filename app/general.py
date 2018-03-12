64d42be21919b2eae1b3407b4684265108430b87
rudeak <rudeak@gmail.com>
2018-03-12 14:43:43 +0200

    костиль в статистиці

--- app/general.py
+++ app/general.py
@@ -4,4 +4,19 @@ from app import db
 
 
 def stats ():
-    return {'users':User.query.all().count(), 'games_active':Game.query.all().count(), 'games_played':ArchiveGame.query.all.count(), 'active_proxy':Proxy.query.all().count()}
+    users = 0
+    for user in User.query.all():
+        users +=1
+    games_active = 0
+    for game in Game.query.all():
+        games_active+=1
+    games_played = 0
+    for played in ArchiveGame.query.all():
+        games_played +=1
+    active_proxy = 0
+    for proxy in Proxy.query.all():
+        active_proxy +=1
+    return {'users':users, 
+            'games_active':games_active,
+            'games_played':games_played,
+            'active_proxy':active_proxy}
