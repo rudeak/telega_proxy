d81297dba011874df92b97cede3c181430000b71
rudeak <rudeak@gmail.com>
2018-03-12 16:39:17 +0200

    parser added

---
+++ app/proxy/parser.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+
+"""
+left_panel_tag ='tdContentLeft'
+header_tag = 'tableHeader'
+right_panel_tag ='tdContentRight'
+main_manu_tag = 'menuWrap'
+info_block ='boxCenterContent'
+vote_class ='enPnl1 border_rad2 clr2'
+discuss_class ='discuss'
+game_info_class =  'lnkGameTitle' #'boxGameInfo' 'yellow_darkgreen19'
+game_info_table_class = 'gameInfo'
+"""
+
+game_name_id = 'lnkGameTitle'
+
+def get_game_info(page):
+    soup = BeautifulSoup(page.text)
+    soup.find (id = game_name_id)
+    content = soup.prettify()
+    print (content)
+    return content
--- app/proxy/proxy.py
+++ app/proxy/proxy.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, flash, redirect, session, url_for,
 from jinja2 import TemplateNotFound
 from flask_login import login_user, logout_user, current_user, login_required
 import app
+from parser import get_game_info
 
 
 
@@ -19,5 +20,5 @@ def show():
 def proxy_creator(id):
     r = requests.Session()
     page = r.get('http://quest.ua/GameDetails.aspx?gid='+str(id))
-    print (page.text)
+    get_game_info (page)
     return redirect(url_for(request.args.get ("redirect_url")))
