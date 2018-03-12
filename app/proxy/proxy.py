4515d7dfda6fedb110c7cb6f27f952a2269edc2b
rudeak <rudeak@gmail.com>
2018-03-12 15:41:30 +0200

    request.args

--- app/proxy/proxy.py
+++ app/proxy/proxy.py
@@ -14,6 +14,6 @@ def show():
 
 @proxy.route('/proxy_creator/<id>')
 @login_required
-def proxy_creator (id, redirect_url):
+def proxy_creator (id):
     print ('game id ='+ str(id))
-    return redirect(url_for(redirect_url))
+    return request.args 
