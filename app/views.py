33af3ec60065077944d835e966247c39e474c425
rudeak <rudeak@gmail.com>
2018-03-12 15:08:08 +0200

    proxy.proxy_creator(game.game_id)

--- app/views.py
+++ app/views.py
@@ -156,7 +156,7 @@ def game_creator():
     print ('views')
     print (game)
     proxy_db (game.game_id)
-    redirect (url_for ('proxy.proxy_creator'))
+    redirect (url_for ('proxy.proxy_creator(game.game_id)'))
     return redirect(url_for('active_games_list_tmplt'))
 
 
