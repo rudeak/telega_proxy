773b9303d0cba03560b8f6eeacbd48189fb3b98d
rudeak <rudeak@gmail.com>
2018-03-22 14:51:01 +0200

    str(s)

--- app/proxy/parser.py
+++ app/proxy/parser.py
@@ -126,7 +126,7 @@ def get_sectors (pageSoup):
     content = pageSoup.find ('div', class_ = content_div_class)
     sectors_count = content.findAll ('h3')[0]
     i = 0
-    sectors = [for s in sectors_count.get_text().split() if s.isdigit()]
+    sectors = [str(s) for s in sectors_count.get_text().split() if s.isdigit()]
     print (str(sectors))
     return 1
 
