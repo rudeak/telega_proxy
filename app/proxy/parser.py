800555a34df4b951666a3141de91d46648097d59
rudeak <rudeak@gmail.com>
2018-03-23 15:29:15 +0200

    timers parser

--- app/proxy/parser.py
+++ app/proxy/parser.py
@@ -188,9 +188,11 @@ def get_task(pageSoup):
 def get_prompts (pageSoup):
     content = pageSoup.find('div', class_ = content_div_class)
     blocks = BeautifulSoup (set_block(content.prettify()))
-    prompts = blocks.find('div', class_ = 'block_prompt')
+    prompts = blocks.findAll('div', class_ = 'block_prompt')
     for prompt in prompts:
-        print (prompt)
+        promptSoup = BeautifulSoup (prompt)
+        if len(promptSoup.findAll('div', class_ = code_not_entered_class)): != 0:
+            print (prompt)
     return 1
 
 def set_block (html):
