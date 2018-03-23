f35a89933f7c114aef29c8b5a6bae3343dcdf453
rudeak <rudeak@gmail.com>
2018-03-23 13:44:18 +0200

    dfsdf

--- app/proxy/parser.py
+++ app/proxy/parser.py
@@ -63,7 +63,7 @@ def level_parser (page):
         print(get_sectors_count (soup))
         print (get_sectors_info (soup))
     print(get_task(soup))
-    get_promts (pageSoup)
+    get_promts (soup)
     return page
 
 def get_level_num (pageSoup):
