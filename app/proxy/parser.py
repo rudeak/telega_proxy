0a5a08d274ee15960a3e08394693743137eea341
rudeak <rudeak@gmail.com>
2018-03-26 15:38:21 +0300

    dsfsdf

--- app/proxy/parser.py
+++ app/proxy/parser.py
@@ -262,7 +262,6 @@ def set_block (html):
             i = 1
     html_dic = rename_block (html_dic)   
     html = ''.join(html_dic)
-    print (html)
     return html
 
 def rename_block (html_dic):
