b6dc8b71c9ac60dd8e81fd0d214f10d553892a7c
rudeak <rudeak@gmail.com>
2018-03-26 20:45:33 +0300

    :

--- app/proxy/parser.py
+++ app/proxy/parser.py
@@ -315,8 +315,8 @@ def rename_block (html_dic):
     counter = 0
     for z in range(0, len(html_dic)-1):
         if html_dic[z] == '<div class="block">' and counter == 0:
-            if html_dic[z+1].strip()!='<h3 class="'+timer_class+'">'
-                html_dic[z] = '<div class="block_sectors">': 
+            if html_dic[z+1].strip()!='<h3 class="'+timer_class+'">':
+                html_dic[z] = '<div class="block_sectors">' 
                 counter = 1
             else:
                 html_dic[z] = '<div class="block_timer">'
