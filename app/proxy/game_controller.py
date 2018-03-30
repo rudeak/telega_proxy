aa71793bc23e1f23d5196cc143c2fe4a3e0cc8fc
rudeak <rudeak@gmail.com>
2018-03-30 10:54:43 +0300

    new game scenario

--- app/proxy/game_controller.py
+++ app/proxy/game_controller.py
@@ -10,6 +10,15 @@ def en_game_info_create(page, user_id):
 
 def en_game_logger (proxy_key, page_json):
     print (page_json)
-    print ('game id ='+str(get_game_id(proxy_key)))
+    #print ('game id ='+str(get_game_id(proxy_key)))
+    if EnGame.query.filter_by (en_game_id = get_game_id(proxy_key), proxy_key = proxy_key).count()==0:
+        print ('new game found')
+        game = EnGame (get_game_id(proxy_key), proxy_key)
+        db.session.add (game)
+        try:
+            db.session.commit()
+        except:
+            print ('помилка створення нового сценарю гри')
+            db.session.rollback()
     return 1
 
