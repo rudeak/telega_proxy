08d3dcf6f23590c687da1db5ee0851c8fed5b6a7
rudeak <rudeak@gmail.com>
2018-03-31 23:42:39 +0300

    bonuse paring correction

--- app/proxy/parser.py
+++ app/proxy/parser.py
@@ -389,7 +389,7 @@ def rename_block (html_dic):
         if html_dic[z] == '<div class="block">' and counter == 1:
             html_dic[z] = '<div class="block_task">' 
             counter = 2
-        if html_dic[z] == '<div class="block">' and counter == 2:
+        if html_dic[z] == '<div class="block">' and counter == 2 and ( html_dic[z+1].strip() != '<h3 class="'+correct_bonus_class+'">' or html_dic[z+1].strip() != '<h3 class="'+code_entered_class+'">'):
           if html_dic[z+1].strip() != '<h3 class="'+penalty_h3_class+'">':
                 if penalty:
                    html_dic[z] = '<div class="block_penalty">' 
