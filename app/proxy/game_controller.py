8340c29bae6ebc2f6aee6ba1b2c89c2cc90ae91f
rudeak <rudeak@gmail.com>
2018-04-05 15:30:35 +0300

    sectorsJson

--- app/proxy/game_controller.py
+++ app/proxy/game_controller.py
@@ -69,6 +69,6 @@ def en_level_info_updater (proxy_key, pageJson):
     return None
 
 def en_sectors_logger (proxy_key, en_lvl_id, en_lvl_no, sectorsJson):
-    print ('sectors' + str(sectors_counter))
+    print ('sectors' + str(sectorsJson))
     return None
 
