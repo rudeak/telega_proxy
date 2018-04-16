b7fa9f696acdef86f135542026ab233bfbb371ba
rudeak <rudeak@gmail.com>
2018-04-16 12:27:40 +0300

    debug

--- app/proxy/parser.py
+++ app/proxy/parser.py
@@ -77,7 +77,7 @@ def change_header (page, id):
     soup.prettify()
     header_soup = soup.find('div', class_="header")
     game = Game.query.filter_by(game_id = get_game_id(id)).first()
-    header_soup.find('h1').replaceWith ('<h1><a href ={}>{}</a></h1>'.format('/proxy'+str(id), game.game_name))
+    header_soup.find('h1')[0] = '<h1><a href ={}>{}</a></h1>'.format('/proxy'+str(id), game.game_name)
     print (header_soup)
     return header_soup
 """
