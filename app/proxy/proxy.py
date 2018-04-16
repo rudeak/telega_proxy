f4cbbd55e00797f8c854568d10f63ae415e47f3d
rudeak <rudeak@gmail.com>
2018-04-16 13:26:45 +0300

    #edit page

--- app/proxy/proxy.py
+++ app/proxy/proxy.py
@@ -45,13 +45,14 @@ def proxy_creator(id):
 def en_game_proxy_root(id):
     r = get_session (id)
     url = 'http://'+get_domain(id)+'/gameengines/encounter/play/'+get_game_id(id)
-    en_game_logger(id,level_parser (change_href(r.get (url),id))['json'])
+    page = level_parser (change_href(r.get (url),id))
+    en_game_logger(id,page['json'])
     game = Game.query.filter_by (game_id = get_game_id(id)).first()
     return render_template ('proxy.html', 
             game_stats = game.game_domain + '/GameStat.aspx?gid='+str(get_game_id(id)),
             game_name = game.game_name, 
             alt_game_stats = 'questtools.herokuapp.com/gamestat/'+str (str(get_game_id(id))+'?domain='+game.game_domain, 
-            content = level_parser (change_href(r.get (url),id))['html'])
+            content = page['html'])
   #  level_parser (change_href(r.get (url),id))['html'] # change_href(r.get (url),id) #
 
 
