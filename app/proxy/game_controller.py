ae21b5605b9c8815ff9863ab2da2ca785306c85d
rudeak <rudeak@gmail.com>
2018-04-16 16:18:00 +0300

    №вуигп зкщьзеі вудуешщт куьщмув

--- app/proxy/game_controller.py
+++ app/proxy/game_controller.py
@@ -327,9 +327,7 @@ def print_prompts_from_db (proxy_key, en_lvl_id, en_lvl_no):
     for prompt in en_prompt:
         print (prompt)
     print ('------------------------END DB prompts printing -------------------')
-    for prompt in en_prompt:
-        db.session.delete(prompt)
-        db.session.commit()
+
     return None
                                   
 def en_bonus_logger (proxy_key, en_lvl_id, en_lvl_no, pageJson):
