cb026341f113ac4c41bfce0bf379642c8c50bc30
rudeak <rudeak@gmail.com>
2018-04-16 14:15:34 +0300

    post

--- app/proxy/proxy.py
+++ app/proxy/proxy.py
@@ -91,8 +91,8 @@ def en_game_proxy_post_root(id):
         #for k,v in request.form.to_dict():
         #    post_data[k] = v.encode('utf-8')
         #level_parser (change_href(r.get (url),id))
-        #post = change_href(), id)
-        page = level_parser (r.post (url, request.form.to_dict()))
+        post = change_href(r.post (url, request.form.to_dict()), id)
+        page = level_parser (post)
         en_game_logger(id,page['json'])
         game = Game.query.filter_by (game_id = get_game_id(id)).first()
         alt_game_stats = 'questtools.herokuapp.com/gamestat/'+str(get_game_id(id))+'?domain='+game.game_domain
