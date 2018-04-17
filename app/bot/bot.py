64573eb47e1534303cf38fb0a472138af7fbe042
rudeak <rudeak@gmail.com>
2018-04-17 14:30:17 +0300

    bot edit

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, flash, redirect, session, url_for,
 from jinja2 import TemplateNotFound
 from flask_login import login_user, logout_user, current_user, login_required
 import app
+import telepot
 
 bot = Blueprint('bot', 'bot', template_folder='templates')
 
@@ -9,6 +10,6 @@ telegram_api_key = '598589123:AAH3gPKY_4kvA50wrXQdnq6pGMd14TQId0E'
 
 telega_bot = telepot.Bot(telegram_api_key)
 
-@bot.route('/'+telegram_api_key)
+@bot.route('/'+telegram_api_key, methods = ['POST', 'GET'])
 def message_listener():
     return render_template ('index.html', user = current_user)
