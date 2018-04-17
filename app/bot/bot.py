72ce233ecf65aacc9ef04242e8ca205ef054d793
rudeak <rudeak@gmail.com>
2018-04-17 15:23:12 +0300

    webhook edit

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -9,7 +9,10 @@ bot = Blueprint('bot', 'bot', template_folder='templates')
 telegram_api_key = '598589123:AAH3gPKY_4kvA50wrXQdnq6pGMd14TQId0E'
 
 telega_bot = telepot.Bot(telegram_api_key)
+telega_bot.setWebhook ('https://rudeak.gq/bot/{}'.format(telegram_api_key))
 
 @bot.route('/'+telegram_api_key, methods = ['POST', 'GET'])
 def message_listener():
+    update = request.get_json()
+    print (update)
     return 'bot ok'
