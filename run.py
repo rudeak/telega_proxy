f63f0a3bde86653fa322d921049f756e0efe2dac
rudeak <rudeak@gmail.com>
2018-04-17 14:58:16 +0300

    run.py on 443 port

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!bin/python
 
 from app import app
-app.run(host='0.0.0.0', debug = True, ssl_context=('fullchain.pem', 'privkey.pem', port = 443)
+app.run(host='0.0.0.0', debug = True, ssl_context=('fullchain.pem', 'privkey.pem'), port = 443)
