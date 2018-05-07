61c42e926756b7a95f32de6eee22a3c72d3ae447
rudeak <rudeak@gmail.com>
2018-05-07 11:19:04 +0300

    #debug

--- app/proxy/game_controller.py
+++ app/proxy/game_controller.py
@@ -286,14 +286,13 @@ def en_prompts_loger(proxy_key, en_lvl_id, en_lvl_no, pageJson):
     print(len(prompts))
     # якщо немає піказок тоді вернутись
     if len(prompts) == 0:
-        print ('returned from prompts')
+        
         return None
-    else:
-        print ('note returned from prompts')
     # якщо не створені підказки в базі то створити їх
     if EnPrompt.query.filter_by(en_game_id=get_game_id(proxy_key),
                                 en_lvl_id=en_lvl_id,
                                 en_lvl_no=en_lvl_no).count() == 0:
+        print ('there no prompts in DB')
         for prompt in prompts:
             if prompt['timer'] == '':
                 prompt['timer'] = '0'
