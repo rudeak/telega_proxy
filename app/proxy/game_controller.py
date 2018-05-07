eef7e81ea690d3de75c3625617bb32cb75337cf5
rudeak <rudeak@gmail.com>
2018-05-07 12:18:19 +0300

    #debug

--- app/proxy/game_controller.py
+++ app/proxy/game_controller.py
@@ -299,10 +299,11 @@ def en_prompts_loger(proxy_key, en_lvl_id, en_lvl_no, pageJson):
             if prompt['timer'] == '':
                 prompt['timer'] = '0'
             en_prompt = EnPrompt(get_game_id(proxy_key), en_lvl_id, en_lvl_no, prompt['number'], prompt['text'], int(prompt['timer']))
+            print (en_prompt)
             db.session.add(en_prompt)
             db.session.commit()
             print (prompt)
-            print (en_prompt)
+            
             #if prompt['timer'] =='0':
             #    addSignal (proxy_key, 12, level = en_lvl_no, number = prompt['number'], text = prompt['text'])
             #else:
